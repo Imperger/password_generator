f134f93cd26c5c9ff17aa19bb0634bfef0dff7d5
Imperger <Imperger@users.noreply.github.com>
2021-10-13 01:28:04 +0000

    deploy: 7064d3ec1b7945121b34d59a23f85a8f473757f8

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 self.addEventListener('fetch', e => {
   e.respondWith((async () => {
     const url = new URL(e.request.url);
-
     if (e.request.method === 'GET' &&
             url.origin === location.origin &&
             !url.pathname.includes('.')) {
-      url.pathname = '/index.html';
+      const basePath = self.location.pathname.slice(0, self.location.pathname.lastIndexOf('/') + 1);
+      url.pathname = `${basePath}index.html`;
       return fetch(url.href);
     }
 
